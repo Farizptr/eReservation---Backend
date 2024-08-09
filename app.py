3c82d7b91e670d311fb9b75969b10403202e7400
Farizptr <18221015@std.stei.itb.ac.id>
2024-08-09 10:06:51 +0700

    setup environment

--- app.py
+++ app.py
@@ -3,11 +3,26 @@ from weasyprint import HTML
 from num2words import num2words
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+from dotenv import load_dotenv
 
 # Initialize logging
+load_dotenv()
 
 app = Flask(__name__)
-cred = credentials.Certificate("./ereservation-f5852-firebase-adminsdk-fhiyz-3df40c73aa.json")
+cred = credentials.Certificate({
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+})
 firebase_admin.initialize_app(cred)
 # Initialize Firestore DB
 db = firestore.client()
